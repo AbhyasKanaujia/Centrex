2397f76ae5bc3ac2aa0467f14ca7a475567c6302
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-11 23:33:00 +0530

    Add MorganMiddleware with colors

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 require("dotenv").config();
-const morgan = require("morgan");
+const MorganMiddleware = require("./middlewares/MorganMiddleware.js");
 const bookRoutes = require("./routes/bookRoutes.js");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-if (process.env.NODE_ENV !== "production") app.use(morgan("tiny"));
+app.use(MorganMiddleware);
 
 app.use("/api/books", bookRoutes);
 
