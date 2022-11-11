bd10f95208d53a253754aad58c354e369a3b4da5
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-12 00:06:58 +0530

    add async handler

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,47 +1,50 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc    Get All Books
 // @route   GET /api/books
 // @access  Public
-const getAllBooks = (req, res) => {
-  res.status(200).json({ message: "Get all books" });
-};
+const getAllBooks = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Get all books' })
+})
 
 // @desc    Add a book
 // @route   POST /api/books
 // @access  Private
-const addBook = (req, res) => {
+const addBook = asyncHandler(async (req, res) => {
   if (
     !req.body.title ||
     !req.body.author ||
     !req.body.price ||
     !req.body.isbn
   ) {
-    res.status(400);
+    res.status(400)
 
-    throw new Error("Please add all fields");
+    throw new Error('Please add all fields')
   }
 
-  res.status(201).json({ message: "Add a book" });
-};
+  res.status(201).json({ message: 'Add a book' })
+})
 
 // @desc    Get a book
 // @route   GET /api/books/:id
 // @access  Public
-const getBook = (req, res) => {
-  res.status(200).json({ message: `Add a book with id ${req.params.id}` });
-};
+const getBook = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Add a book with id ${req.params.id}` })
+})
 
 // @desc    Update a book
 // @route   POST /api/goals/:id
 // @access  Private
-const updateBook = (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+const updateBook = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` })
+})
+
 // @desc    Delete a book
 // @route   POST /api/goals/:id
 // @access  Private
-const deleteBook = (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+const deleteBook = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` })
+})
 
 module.exports = {
   getAllBooks,
@@ -49,4 +52,4 @@ module.exports = {
   getBook,
   updateBook,
   deleteBook,
-};
+}
