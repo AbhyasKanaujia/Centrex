9dc961011ceb74c9a7e4842a2f1e485dfa7c29da
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-11 23:22:20 +0530

    Add morgan for logging

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 require("dotenv").config();
+const morgan = require("morgan");
 const bookRoutes = require("./routes/bookRoutes.js");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+if (process.env.NODE_ENV !== "production") app.use(morgan("tiny"));
 
 app.use("/api/books", bookRoutes);
 
