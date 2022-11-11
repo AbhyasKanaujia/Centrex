8d6975b8426e5d9270b167dabb04c45c0dc370b7
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-11 23:02:35 +0530

    Add environment file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(
+    `Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`
+  )
+);
