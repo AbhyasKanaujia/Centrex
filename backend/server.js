4fbb95385cda2e54c85d98c3b07defeb4fd94479
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-12 00:47:45 +0530

    attach user routes to app

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const { errorHandler } = require('./middlewares/errorHandler.js')
 const MorganMiddleware = require('./middlewares/MorganMiddleware.js')
 const bookRoutes = require('./routes/bookRoutes.js')
+const userRoutes = require('./routes/userRoutes.js')
 const connectDB = require('./configs/db.js')
 
 const PORT = process.env.PORT || 5000
@@ -18,6 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/books', bookRoutes)
+app.use('/api/user', userRoutes)
 
 app.use(errorHandler)
 
