0670c7d29a007922362615c99851c57d9b3da761
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-12 16:30:41 +0530

    Add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
-  return <div>My App</div>
+  return (
+    <>
+      <Router>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Dashboard />} exact />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Container>
+      </Router>
+    </>
+  )
 }
 
 export default App
