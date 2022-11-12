94457b4393cf0c5d19b909e3c9388f7fee57f25b
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-12 18:02:38 +0530

    user register service in auth slice

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authService from './authService'
 
 const user = JSON.parse(localStorage.getItem('user'))
 
@@ -12,7 +13,11 @@ const initialState = {
 
 export const register = createAsyncThunk(
   'auth/register',
-  async (user, thunkAPI) => {}
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user)
+    } catch (error) {}
+  }
 )
 
 export const authSlice = createSlice({
