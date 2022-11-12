0e6f542158d52c5aaa238c6bcffbdcc0944e7346
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-12 19:19:59 +0530

    handle mismatch confirm password with a toast

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -6,6 +6,7 @@ import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/esm/Col'
 import Button from 'react-bootstrap/esm/Button'
 import Toast from 'react-bootstrap/esm/Toast'
+import ToastContainer from 'react-bootstrap/esm/ToastContainer'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -19,6 +20,17 @@ function Register() {
     password: '',
     confirmPassword: '',
   })
+
+  const toastInitialState = {
+    visibility: false,
+    title: '',
+    message: '',
+    variant: 'primary',
+  }
+
+  const [toast, setToast] = useState(toastInitialState)
+
+  const dismissToast = () => setToast({ visibility: false })
 
   const { user, isLoading, isSuccess, isError, message } = useSelector(
     (store) => store.auth
@@ -45,6 +57,14 @@ function Register() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+    if (password !== confirmPassword) {
+      setToast({
+        visibility: true,
+        title: 'Passwords do not match',
+        message: 'Please try entering passwords again',
+        variant: 'danger',
+      })
+    }
   }
 
   return (
@@ -55,109 +75,127 @@ function Register() {
         </h1>
         <p className="text-center">Please create an account</p>
       </Row>
-      <Row className="justify-content-center">
-        <Form as={Col} md="8" lg="6" onSubmit={(e) => onSubmit(e)}>
-          <Form.Group>
-            <Form.Control
-              type="text"
-              placeholder="Name"
-              className="my-2"
-              name="name"
-              value={name}
-              onChange={(e) => onChange(e)}
-            />
-          </Form.Group>
-          <Row>
-            <Form.Group as={Col}>
-              <Form.Control
-                type="email"
-                placeholder="Email"
-                className="my-2"
-                name="email"
-                value={email}
-                onChange={(e) => onChange(e)}
-              />
-            </Form.Group>
-            <Form.Group as={Col}>
-              <Form.Control
-                type="tel"
-                placeholder="Phone Number"
-                className="my-2"
-                name="phone"
-                value={phone}
-                onChange={(e) => onChange(e)}
-              />
-            </Form.Group>
-          </Row>
-          <Form.Group>
-            <Form.Control
-              type="text"
-              placeholder="Address Line 1"
-              className="my-2"
-              name="line1"
-              value={line1}
-              onChange={(e) => onChange(e)}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Control
-              type="text"
-              placeholder="Address Line 2"
-              className="my-2"
-              name="line2"
-              value={line2}
-              onChange={(e) => onChange(e)}
-            />
-          </Form.Group>
-          <Row>
-            <Form.Group as={Col}>
+      <Row>
+        <Form onSubmit={(e) => onSubmit(e)}>
+          <Col md="8" lg="6" className="mx-auto">
+            <Form.Group>
               <Form.Control
                 type="text"
-                placeholder="City"
+                placeholder="Name"
                 className="my-2"
-                name="city"
-                value={city}
+                name="name"
+                value={name}
                 onChange={(e) => onChange(e)}
               />
             </Form.Group>
-            <Form.Group as={Col}>
+            <Row>
+              <Form.Group as={Col}>
+                <Form.Control
+                  type="email"
+                  placeholder="Email"
+                  className="my-2"
+                  name="email"
+                  value={email}
+                  onChange={(e) => onChange(e)}
+                />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Control
+                  type="tel"
+                  placeholder="Phone Number"
+                  className="my-2"
+                  name="phone"
+                  value={phone}
+                  onChange={(e) => onChange(e)}
+                />
+              </Form.Group>
+            </Row>
+            <Form.Group>
               <Form.Control
                 type="text"
-                placeholder="State"
+                placeholder="Address Line 1"
                 className="my-2"
-                name="state"
-                value={state}
+                name="line1"
+                value={line1}
                 onChange={(e) => onChange(e)}
               />
             </Form.Group>
-          </Row>
-          <Row>
-            <Form.Group as={Col}>
+            <Form.Group>
               <Form.Control
-                type="password"
-                placeholder="Password"
+                type="text"
+                placeholder="Address Line 2"
                 className="my-2"
-                name="password"
-                value={password}
+                name="line2"
+                value={line2}
                 onChange={(e) => onChange(e)}
               />
             </Form.Group>
-            <Form.Group as={Col}>
-              <Form.Control
-                type="password"
-                placeholder="Confirm Password"
-                className="my-2"
-                name="confirmPassword"
-                value={confirmPassword}
-                onChange={(e) => onChange(e)}
-              />
+            <Row>
+              <Form.Group as={Col}>
+                <Form.Control
+                  type="text"
+                  placeholder="City"
+                  className="my-2"
+                  name="city"
+                  value={city}
+                  onChange={(e) => onChange(e)}
+                />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Control
+                  type="text"
+                  placeholder="State"
+                  className="my-2"
+                  name="state"
+                  value={state}
+                  onChange={(e) => onChange(e)}
+                />
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group as={Col}>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  className="my-2"
+                  name="password"
+                  value={password}
+                  onChange={(e) => onChange(e)}
+                />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm Password"
+                  className="my-2"
+                  name="confirmPassword"
+                  value={confirmPassword}
+                  onChange={(e) => onChange(e)}
+                />
+              </Form.Group>
+            </Row>
+            <Form.Group className="my-3">
+              <Button type="submit" className="w-100">
+                Create an account
+              </Button>
             </Form.Group>
-          </Row>
-          <Form.Group className="my-3">
-            <Button className="w-100">Create an account</Button>
-          </Form.Group>
+          </Col>
         </Form>
       </Row>
+      <ToastContainer position="top-center" className="p-3">
+        <Toast
+          show={toast.visibility}
+          bg={toast.variant}
+          onClose={dismissToast}
+          delay={3000}
+          autohide
+        >
+          <Toast.Header>
+            <strong className="me-auto">{toast.title}</strong>
+          </Toast.Header>
+          <Toast.Body>{toast.message}</Toast.Body>
+        </Toast>
+      </ToastContainer>
     </>
   )
 }
