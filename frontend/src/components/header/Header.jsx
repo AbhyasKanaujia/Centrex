625d50bbaf68897bce3be8ac6031ea7dd3b26b0f
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-14 22:57:44 +0530

    design hero

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,75 +3,78 @@ import logo from '../../logo.svg'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import { CiLogin, CiUser } from 'react-icons/ci'
+import { Container } from 'react-grid-system'
 
 const Header = () => {
   const [activeLinkName, setActiveLinkName] = useState('Home')
 
   return (
     <div className="header">
-      <Link
-        to="/"
-        className="header__brand"
-        onClick={() => setActiveLinkName('Home')}
-      >
-        <img
-          className="header__brand-logo"
-          src={logo}
-          alt="Centrex Logo"
-          width={48}
-        />
-        <p className="header__brand-title">centrex</p>
-      </Link>
-      <div>
-        <ul>
-          <li className="header__nav-item">
-            <Link
-              className={`header__nav-link ${
-                activeLinkName === 'Home' ? 'header__nav-link--active' : ''
-              }`}
-              activeClassName="header__nav-link--active"
-              to="/"
-              onClick={() => setActiveLinkName('Home')}
-            >
-              Home
-            </Link>
-          </li>
-          <li className="header__nav-item">
-            <Link
-              className={`header__nav-link ${
-                activeLinkName === 'Book' ? 'header__nav-link--active' : ''
-              }`}
-              activeClassName="header__nav-link--active"
-              to="/books"
-              onClick={() => setActiveLinkName('Book')}
-            >
-              Books
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <ul>
-          <li className="header__menu-item">
-            <Link
-              className="header__menu-link"
-              to="/login"
-              onClick={() => setActiveLinkName('')}
-            >
-              <CiLogin /> Login
-            </Link>
-          </li>
-          <li className="header__menu-item">
-            <Link
-              className="header__menu-link"
-              to="/register"
-              onClick={() => setActiveLinkName('')}
-            >
-              <CiUser /> Register
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <Container className="header__container">
+        <Link
+          to="/"
+          className="header__brand"
+          onClick={() => setActiveLinkName('Home')}
+        >
+          <img
+            className="header__brand-logo"
+            src={logo}
+            alt="Centrex Logo"
+            width={48}
+          />
+          <p className="header__brand-title">centrex</p>
+        </Link>
+        <div>
+          <ul>
+            <li className="header__nav-item">
+              <Link
+                className={`header__nav-link ${
+                  activeLinkName === 'Home' ? 'header__nav-link--active' : ''
+                }`}
+                activeClassName="header__nav-link--active"
+                to="/"
+                onClick={() => setActiveLinkName('Home')}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link
+                className={`header__nav-link ${
+                  activeLinkName === 'Book' ? 'header__nav-link--active' : ''
+                }`}
+                activeClassName="header__nav-link--active"
+                to="/books"
+                onClick={() => setActiveLinkName('Book')}
+              >
+                Books
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li className="header__menu-item">
+              <Link
+                className="header__menu-link"
+                to="/login"
+                onClick={() => setActiveLinkName('')}
+              >
+                <CiLogin /> Login
+              </Link>
+            </li>
+            <li className="header__menu-item">
+              <Link
+                className="header__menu-link"
+                to="/register"
+                onClick={() => setActiveLinkName('')}
+              >
+                <CiUser /> Register
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </Container>
     </div>
   )
 }
--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -1,7 +1,25 @@
 import './hero.css'
+import { Container } from 'react-grid-system'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
-  return <div>Hero</div>
+  return (
+    <div className="hero">
+      <Container className="hero__container">
+        <div className="hero__content">
+          <h1 className="hero__content-title">
+            For all student needs in one place
+          </h1>
+          <p className="hero__content-text ">
+            From books to laundary to tiffin and more.
+          </p>
+          <Link to="#services" as="button" className="hero__content-button">
+            Show Items
+          </Link>
+        </div>
+      </Container>
+    </div>
+  )
 }
 
 export default Hero
