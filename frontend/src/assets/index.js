6271e1603d247abc4bfa9bd192e8ba6248301e09
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-15 20:20:10 +0530

    design footer

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -2,5 +2,6 @@ import { default as books } from './books.jpg'
 import { default as hero } from './hero.jpg'
 import { default as laundry } from './laundry.jpg'
 import { default as tiffin } from './tiffin.jpg'
+import { default as collegeLogo } from './college_logo.png'
 
-export { books, hero, laundry, tiffin }
+export { books, hero, laundry, tiffin, collegeLogo }
--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,7 +1,21 @@
 import './footer.css'
 
+import { collegeLogo } from '../../assets'
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="footer">
+      <div className="footer__content">
+        <img
+          src={collegeLogo}
+          className="footer__content-college-logo"
+          alt="National Institute of Technology Patna"
+        />
+        <p>National Institute of Technology Patna</p>
+        <p>Project by Abhyas Kumar kanaujia and Khushboo Gupta</p>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
